45f313ca37e17e11877f79187a55d7bd6a4556d1
Charles Broskoski <broskoski@gmail.com>
2015-08-12 16:59:56 -0400

    use slack api, return with api response

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 gem 'sinatra'
-gem 'slack-ruby-client'
+gem 'slack-api'
 gem 'dotenv'
+gem 'httparty'
--- app.rb
+++ app.rb
@@ -1,29 +1,35 @@
 # app.rb
 require 'sinatra'
-require 'slack-ruby-client'
 require 'dotenv'
+require 'slack'
+require 'json'
+require 'httparty'
+require 'pp'
+
 Dotenv.load
 
 class WhoIs < Sinatra::Base
 
   post '/' do
-    Slack::Web::Client.config do |config|
+    content_type :json
+
+    Slack.configure do |config|
       config.token = ENV['SLACK_API_TOKEN']
-      config.user_agent = 'Slack Ruby Client/1.0'
-      puts config.token
     end
 
-    @client = Slack::Web::Client.new
+    client = Slack::Client.new
 
     subject = params[:text]
     user = params[:user_name]
 
-    @client.users.list.members.each do |user|
+    client.users_list['members'].each do |user|
       if user["name"] == subject
-        puts user
+        response = HTTParty.get("#{ENV['TEAM_NAV_API']}member?=#{user['profile']['email']}")
+        puts "#{ENV['TEAM_NAV_API']}member?=#{user['profile']['email']}"
+        puts response.body
+        response.body.to_json
       end
     end
-
   end
 
 end
