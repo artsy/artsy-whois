cd84c0a400ac0d9d2bb75a1ceb10dae4928e9db5
Charles Broskoski <broskoski@gmail.com>
2015-08-13 10:53:59 -0400

    use find

--- app.rb
+++ app.rb
@@ -22,51 +22,35 @@ class WhoIs < Sinatra::Base
     subject = params[:text]
     user_name = params[:user_name]
 
-    client.users_list['members'].each do |user|
-      if user["name"] == subject
+    user = client.users_list['members'].find { |u| user["name"] == subject }
 
-        response = HTTParty.get("#{ENV['TEAM_NAV_API']}member?=#{user['profile']['email']}")
-        artsy_user = JSON.parse(response.body)
+    response = HTTParty.get("#{ENV['TEAM_NAV_API']}member?=#{user['profile']['email']}")
+    artsy_user = JSON.parse(response.body)
 
-        attachments = [{
-            title: "#{artsy_user['name']}",
-            text: "",
-            thumb_url: user['profile']['image_192'],
-            fields: [
-              {
-                title: "Role",
-                value: "#{artsy_user['role']}",
-                short: true
-              },
-              {
-                title: "Team",
-                value: "#{artsy_user['practice']}",
-                short: true
-              }
-            ]
-          }]
-        args = {
-          channel: "@#{user_name}",
-          text: "",
-          icon_url: "https://www.artsy.net/images/icon-150.png",
-          attachments: attachments.to_json
-        }
-        client.chat_postMessage args
-      end
-    end
-  end
-end
-
-class ArtsyUsers
-
-  def initialize
-    @csv = CSV.open(ENV['TEAM_CSV'], :headers => true).map { |x| x.to_h }
-    @users = @csv.to_json
-    puts @users
-  end
-
-  def get(attr)
+    attachments = [{
+        title: "#{artsy_user['name']}",
+        text: "",
+        thumb_url: user['profile']['image_192'],
+        fields: [
+          {
+            title: "Role",
+            value: "#{artsy_user['role']}",
+            short: true
+          },
+          {
+            title: "Team",
+            value: "#{artsy_user['practice']}",
+            short: true
+          }
+        ]
+      }]
+    args = {
+      channel: "@#{user_name}",
+      text: "",
+      icon_url: "https://www.artsy.net/images/icon-150.png",
+      attachments: attachments.to_json
+    }
+    client.chat_postMessage args
 
   end
-
 end
