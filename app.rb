bb8640c8d7e40f302e2383c4c82acafc92d60076
Charles Broskoski <broskoski@gmail.com>
2015-08-13 10:47:36 -0400

    formats respones

--- app.rb
+++ app.rb
@@ -5,11 +5,11 @@ require 'slack'
 require 'json'
 require 'httparty'
 require 'pp'
+require 'csv'
 
 Dotenv.load
 
 class WhoIs < Sinatra::Base
-
   post '/' do
     content_type :json
 
@@ -20,19 +20,53 @@ class WhoIs < Sinatra::Base
     client = Slack::Client.new
 
     subject = params[:text]
-    user = params[:user_name]
+    user_name = params[:user_name]
 
     client.users_list['members'].each do |user|
       if user["name"] == subject
+
         response = HTTParty.get("#{ENV['TEAM_NAV_API']}member?=#{user['profile']['email']}")
-        puts "#{ENV['TEAM_NAV_API']}member?=#{user['profile']['email']}"
-        puts response.body
-        response.body.to_json
+        artsy_user = JSON.parse(response.body)
 
-        client.chat_postMessage channel: '@christina', text: "#{JSON.parse(response.body)['name']}"
-
+        attachments = [{
+            title: "#{artsy_user['name']}",
+            text: "",
+            thumb_url: user['profile']['image_192'],
+            fields: [
+              {
+                title: "Role",
+                value: "#{artsy_user['role']}",
+                short: true
+              },
+              {
+                title: "Team",
+                value: "#{artsy_user['practice']}",
+                short: true
+              }
+            ]
+          }]
+        args = {
+          channel: "@#{user_name}",
+          text: "",
+          icon_url: "https://www.artsy.net/images/icon-150.png",
+          attachments: attachments.to_json
+        }
+        client.chat_postMessage args
       end
     end
+  end
+end
+
+class ArtsyUsers
+
+  def initialize
+    @csv = CSV.open(ENV['TEAM_CSV'], :headers => true).map { |x| x.to_h }
+    @users = @csv.to_json
+    puts @users
+  end
+
+  def get(attr)
+
   end
 
 end
